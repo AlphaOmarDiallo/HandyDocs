fb727389096e034090c6f3cbed227b5a35808587
Alpha <adiallo@exomind.fr>
2024-08-11 17:04:17 +0200

    remove useless composable

--- app/src/main/java/com/alphaomardiallo/handydocs/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/alphaomardiallo/handydocs/presentation/main/MainViewModel.kt
@@ -1,9 +1,22 @@
 package com.alphaomardiallo.handydocs.presentation.main
 
+import androidx.lifecycle.viewModelScope
+import com.alphaomardiallo.handydocs.domain.model.ImageDoc
 import com.alphaomardiallo.handydocs.domain.navigator.AppNavigator
+import com.alphaomardiallo.handydocs.domain.repository.ImageDocRepository
 import com.alphaomardiallo.handydocs.presentation.base.BaseViewModel
+import kotlinx.coroutines.launch
 
-class MainViewModel(appNavigator: AppNavigator): BaseViewModel(appNavigator) {
+class MainViewModel(
+    appNavigator: AppNavigator,
+    private val imageDocRepository: ImageDocRepository
+): BaseViewModel(appNavigator) {
 
     val navigationChannel = appNavigator.navigationChannel
+
+    fun savePdfInDatabase(pdf: ImageDoc) {
+        viewModelScope.launch {
+            imageDocRepository.upsertImage(pdf)
+        }
+    }
 }
