54f053e4db594978ecd929ed6b559e1ffcffb98b
Alpha <adiallo@exomind.fr>
2024-08-12 01:29:01 +0200

    sharing doc works

--- app/src/main/java/com/alphaomardiallo/handydocs/presentation/home/Home.kt
+++ app/src/main/java/com/alphaomardiallo/handydocs/presentation/home/Home.kt
@@ -1,12 +1,13 @@
 package com.alphaomardiallo.handydocs.presentation.home
 
+import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -16,7 +17,6 @@ import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
@@ -31,15 +31,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.FileProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import com.alphaomardiallo.handydocs.R
 import com.alphaomardiallo.handydocs.domain.model.ImageDoc
 import org.koin.androidx.compose.koinViewModel
+import java.io.File
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = koinViewModel()) {
@@ -91,11 +94,15 @@ private fun ListNotEmptyScreen(list: List<ImageDoc> = emptyList()) {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun ListNotEmptyScreenV2(list: List<ImageDoc> = emptyList()) {
+    val context = LocalContext.current
+
     LazyVerticalStaggeredGrid(
         columns = StaggeredGridCells.Fixed(2),
         verticalItemSpacing = 4.dp,
         horizontalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = Modifier.fillMaxSize().padding(4.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp)
     ) {
         items(list) { photo ->
             Card(
@@ -133,10 +140,42 @@ private fun ListNotEmptyScreenV2(list: List<ImageDoc> = emptyList()) {
                         modifier = Modifier.fillMaxWidth(),
                         horizontalArrangement = Arrangement.SpaceEvenly
                     ) {
-                        IconButton(onClick = { /*TODO*/ }) {
+                        IconButton(onClick = {
+                            // Path to the PDF file in internal storage
+                            val pdfFile = File(photo.uriPdf?.path ?: "")
+
+                            // Check if the file exists
+                            if (pdfFile.exists()) {
+                                // Generate the URI using FileProvider
+                                val pdfUri: Uri = FileProvider.getUriForFile(
+                                    context,
+                                    "${context.packageName}.fileprovider",
+                                    pdfFile
+                                )
+
+                                // Create the share intent
+                                val shareIntent: Intent = Intent().apply {
+                                    action = Intent.ACTION_SEND
+                                    type = "application/pdf"
+                                    putExtra(Intent.EXTRA_STREAM, pdfUri)
+                                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                                }
+
+                                // Start the share intent
+                                context.startActivity(
+                                    Intent.createChooser(shareIntent, "Share PDF")
+                                )
+                            } else {
+                                Toast.makeText(context,
+                                    "PDF file not found",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }) {
                             Icon(
                                 imageVector = Icons.Default.Share,
-                                contentDescription = Icons.Default.Share.name
+                                contentDescription = Icons.Default.Share.name,
+                                tint = MaterialTheme.colorScheme.primary
                             )
                         }
                         IconButton(onClick = { /*TODO*/ }) {
@@ -147,9 +186,7 @@ private fun ListNotEmptyScreenV2(list: List<ImageDoc> = emptyList()) {
                         }
                     }
                 }
-
             }
-
         }
     }
 }
