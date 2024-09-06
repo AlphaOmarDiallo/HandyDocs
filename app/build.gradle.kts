9934bdbc796459c984a2163064bf3b51757ba40e
Alpha <adiallo@exomind.fr>
2024-09-06 14:13:35 +0200

    VERSION UPGRADE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.alphaomardiallo.handydocs"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
