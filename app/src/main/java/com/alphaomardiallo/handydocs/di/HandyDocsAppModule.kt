02c3b2db01683fd09e3f5fbc657deba2193eb289
Alpha <adiallo@exomind.fr>
2025-02-18 11:44:19 +0100

    Dependencies added and some cleaning done.

--- app/src/main/java/com/alphaomardiallo/handydocs/di/HandyDocsAppModule.kt
+++ app/src/main/java/com/alphaomardiallo/handydocs/di/HandyDocsAppModule.kt
@@ -6,27 +6,27 @@ import com.alphaomardiallo.handydocs.common.data.provideImageDao
 import com.alphaomardiallo.handydocs.common.domain.navigator.AppNavigator
 import com.alphaomardiallo.handydocs.common.domain.navigator.AppNavigatorImp
 import com.alphaomardiallo.handydocs.common.domain.repository.ImageDocRepository
+import com.alphaomardiallo.handydocs.common.presentation.main.MainViewModel
 import com.alphaomardiallo.handydocs.feature.docviewer.DocViewerViewModel
 import com.alphaomardiallo.handydocs.feature.pdfsafe.HomeViewModel
-import com.alphaomardiallo.handydocs.common.presentation.main.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     // Main
     single<AppNavigator> { AppNavigatorImp() }
-    viewModel { MainViewModel(get(), get()) }
+    viewModel { MainViewModel(appNavigator = get(), imageDocRepository = get()) }
 
     // Home
-    viewModel { HomeViewModel(get(), get()) }
+    viewModel { HomeViewModel(appNavigator = get(), imageDocRepository = get()) }
 
     // Doc viewer
-    viewModel { DocViewerViewModel(get(), get()) }
+    viewModel { DocViewerViewModel(appNavigator = get(), imageDocRepository = get()) }
 
     // Database
-    single { provideDataBase(get()) }
-    single { provideImageDao(get()) }
+    single { provideDataBase(application = get()) }
+    single { provideImageDao(appDataBase = get()) }
 
     // ImageDoc repository
-    single<ImageDocRepository> { ImageDocRepositoryImp(get()) }
+    single<ImageDocRepository> { ImageDocRepositoryImp(appDataBase = get()) }
 }
