454d13b22ed779394ccaec8fc51c1bfe59bbe615
Alpha <adiallo@exomind.fr>
2025-02-18 10:00:12 +0100

    Dependencies upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.google.gms.google.services) apply false
     alias(libs.plugins.google.firebase.crashlytics) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
