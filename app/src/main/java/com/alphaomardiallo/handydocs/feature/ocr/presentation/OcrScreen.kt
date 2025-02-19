d8cb1e8518d7ce7d178c3c08d06fd6b637663bb8
Alpha <adiallo@exomind.fr>
2025-02-19 16:44:32 +0100

    Works from scanner

--- app/src/main/java/com/alphaomardiallo/handydocs/feature/ocr/presentation/OcrScreen.kt
+++ app/src/main/java/com/alphaomardiallo/handydocs/feature/ocr/presentation/OcrScreen.kt
@@ -1,8 +1,11 @@
 package com.alphaomardiallo.handydocs.feature.ocr.presentation
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.net.Uri
-import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,13 +22,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import com.alphaomardiallo.handydocs.feature.pdfsafe.HomeViewModel.HomeUiState
+import androidx.core.content.ContextCompat.getString
+import com.alphaomardiallo.handydocs.R
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_PDF
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
 import org.koin.androidx.compose.koinViewModel
+import timber.log.Timber
 
 @Composable
-fun OcrScreen(viewModel: OcrViewModel = koinViewModel()){
+fun OcrScreen(viewModel: OcrViewModel = koinViewModel()) {
     val uiState = viewModel.state
     val context = LocalContext.current
+    val activity = context as? Activity
     var selectedFileUri by remember { mutableStateOf<Uri?>(null) }
     var textFieldValue by remember(uiState.text) {
         mutableStateOf(TextFieldValue(uiState.text ?: ""))
@@ -36,9 +47,53 @@ fun OcrScreen(viewModel: OcrViewModel = koinViewModel()){
             selectedFileUri = uri
         }
 
-    Column(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
+    val options = GmsDocumentScannerOptions.Builder()
+        .setGalleryImportAllowed(false)
+        .setResultFormats(RESULT_FORMAT_JPEG)
+        .setScannerMode(GmsDocumentScannerOptions.SCANNER_MODE_FULL)
+        .build()
+
+    val scanner = GmsDocumentScanning.getClient(options)
+    val scannerLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartIntentSenderForResult()) { rawResult ->
+        val result = GmsDocumentScanningResult.fromActivityResultIntent(rawResult.data)
+        if (rawResult.resultCode == RESULT_OK) {
+            activity?.let { nonNullActivity ->
+                result?.pages?.mapNotNull { page ->
+                    viewModel.analyse(page.imageUri, context)
+                }
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
         Button(onClick = { filePickerLauncher.launch(arrayOf("*/*")) }) {
             Text("Select File")
+        }
+        Button(onClick = {
+            activity?.let { nonNullActivity ->
+                scanner.getStartScanIntent(nonNullActivity)
+                    .addOnSuccessListener {
+                        scannerLauncher.launch(
+                            IntentSenderRequest.Builder(it).build()
+                        )
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(
+                            context,
+                            String.format(
+                                getString(context, R.string.toast_error_message),
+                                it.localizedMessage
+                            ),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+            }
+        }) {
+            Text("Take picture")
         }
 
         selectedFileUri?.let {
@@ -53,7 +108,6 @@ fun OcrScreen(viewModel: OcrViewModel = koinViewModel()){
                     textFieldValue = newText
                 }
             )
-            Log.e("OCR", "OCR : $it")
         }
     }
 }
