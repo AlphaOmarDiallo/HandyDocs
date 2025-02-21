7b0c07fed0204ea1170f07829120121f63400fbb
Alpha <adiallo@exomind.fr>
2025-02-21 10:58:20 +0100

    VERSION : 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.alphaomardiallo.handydocs"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.3.0"
+        versionCode = 7
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
