aded036db3f1308145aa6888518be88e4d44371a
Alpha <adiallo@exomind.fr>
2025-02-27 07:38:51 +0100

    sorted dependencies for clarity

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.google.gms.google.services) apply false
     alias(libs.plugins.google.firebase.crashlytics) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
 }
