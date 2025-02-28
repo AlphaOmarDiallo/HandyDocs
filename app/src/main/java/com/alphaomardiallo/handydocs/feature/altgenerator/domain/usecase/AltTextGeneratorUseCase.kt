114914c310547b8ab064238c0d49a3a42d8adc5c
Alpha <adiallo@exomind.fr>
2025-02-28 16:41:37 +0100

    Code reformat

--- app/src/main/java/com/alphaomardiallo/handydocs/feature/altgenerator/domain/usecase/AltTextGeneratorUseCase.kt
+++ app/src/main/java/com/alphaomardiallo/handydocs/feature/altgenerator/domain/usecase/AltTextGeneratorUseCase.kt
@@ -2,7 +2,6 @@ package com.alphaomardiallo.handydocs.feature.altgenerator.domain.usecase
 
 import com.alphaomardiallo.handydocs.common.data.DataResponse
 import com.alphaomardiallo.handydocs.common.domain.model.DomainResponse
-import com.alphaomardiallo.handydocs.feature.altgenerator.data.remote.model.GenerateContentRequest
 import com.alphaomardiallo.handydocs.feature.altgenerator.domain.model.GeminiPrompt
 import com.alphaomardiallo.handydocs.feature.altgenerator.domain.repository.AltGeneratorRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,26 +9,27 @@ import kotlinx.coroutines.flow.flow
 
 class AltTextGeneratorUseCase(private val repository: AltGeneratorRepository) {
 
-    suspend fun invoke(prompt: String, imageBase64: String): Flow<DomainResponse<GeminiPrompt>> = flow {
-        emit(DomainResponse.Loading())
+    suspend fun invoke(prompt: String, imageBase64: String): Flow<DomainResponse<GeminiPrompt>> =
+        flow {
+            emit(DomainResponse.Loading())
 
-        when (val result = repository.generateAltText(prompt, imageBase64)) {
-            is DataResponse.Error -> {
-                emit(
-                    DomainResponse.Error(
-                        errorCode = result.errorCode,
-                        description = result.description
+            when (val result = repository.generateAltText(prompt, imageBase64)) {
+                is DataResponse.Error -> {
+                    emit(
+                        DomainResponse.Error(
+                            errorCode = result.errorCode,
+                            description = result.description
+                        )
                     )
-                )
-            }
+                }
 
-            is DataResponse.Success -> {
-                emit(
-                    DomainResponse.Success(
-                        response = result.response.toDomain()
+                is DataResponse.Success -> {
+                    emit(
+                        DomainResponse.Success(
+                            response = result.response.toDomain()
+                        )
                     )
-                )
+                }
             }
         }
-    }
 }
